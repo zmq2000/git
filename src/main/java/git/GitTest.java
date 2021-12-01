84785d8e9fc1fcc9313378f72671c9e88ec86984
zmq2000 <71876096+zmq2000@users.noreply.github.com>
2021-12-01 20:28:05 +0800

    1

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("4");
         System.out.println("hot-fix");
         System.out.println("push");
+                System.out.println("pull");
     }
 }
