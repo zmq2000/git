c6c5b0b9f8f081c6cac7c62e6f21a183cc2def5d
alsy <1743799182@qq.com>
2021-12-01 20:23:32 +0800

    master commit2

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("master");
         System.out.println("4");
         System.out.println("hot-fix");
+        System.out.println("push");
     }
 }
