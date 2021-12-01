7d46c9623edfdcf26fcacba0cdaa181e55233930
alsy <1743799182@qq.com>
2021-12-01 19:59:52 +0800

    second committ

---
+++ src/main/java/git/GitTest.java
@@ -0,0 +1,11 @@
+package git;
+
+/**
+ * @Description
+ * @Data 2021/12/1 19:58
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
